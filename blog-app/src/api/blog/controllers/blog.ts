4a85e007d40d9d7bfaae4a887a25e45a6a0db7d3
HaShingThang <scm.hashingthang@gmail.com>
2023-03-24 17:11:07 +0630

    add service, route, controller

--- blog-app/src/api/blog/controllers/blog.ts
+++ blog-app/src/api/blog/controllers/blog.ts
@@ -4,4 +4,58 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::blog.blog');
+export default factories.createCoreController('api::blog.blog', ({ strapi }) => ({
+    async find(ctx) {
+        ctx.body = await strapi.service("api::blog.blog").findBlog();
+    },
+
+    async findOne(ctx) {
+        const { id } = ctx.params;
+        try {
+            const blogPost = await strapi.service("api::blog.blog").findOneBlog(id);
+            if (!blogPost) throw Error(`Blog id ${id} not found.`)
+            ctx.body = blogPost;
+
+        } catch (error) {
+            ctx.response.status = 404;
+            ctx.response.body = {
+                message: error.message,
+            };
+        }
+    },
+
+    async create(ctx) {
+        const { title, description } = ctx.request.body;
+        ctx.body = await strapi.service("api::blog.blog").createBlog({ title, description });
+    },
+
+    async update(ctx) {
+        const { id } = ctx.params;
+        console.log(id)
+        const { title, description } = ctx.request.body;
+        try {
+            const updateBlog = await strapi.service("api::blog.blog").updateBlog(id, { title, description });
+            if (!updateBlog) throw Error(`Blog id ${id} not found.`)
+            ctx.body = updateBlog
+        } catch (error) {
+            ctx.response.status = 404;
+            ctx.response.body = {
+                message: error.message,
+            };
+        }
+    },
+
+    async delete(ctx) {
+        const { id } = ctx.params;
+        try {
+            const deleteBlog = await strapi.service("api::blog.blog").deleteBlog(id);
+            if (!deleteBlog) throw Error(`Blog id ${id} not found.`)
+            ctx.body = deleteBlog
+        } catch (error) {
+            ctx.response.status = 404;
+            ctx.response.body = {
+                message: error.message,
+            };
+        }
+    },
+}));
--- blog-app/src/api/blog/services/blog.ts
+++ blog-app/src/api/blog/services/blog.ts
@@ -4,4 +4,41 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::blog.blog');
+export default factories.createCoreService('api::blog.blog', ({ strapi }) => ({
+    async findBlog() {
+        return strapi.entityService.findMany('api::blog.blog', {
+            fields: ['title', 'description', 'createdAt', 'updatedAt'],
+            sort: { createdAt: 'DESC' },
+        });
+    },
+
+    async findOneBlog(id) {
+        return strapi.entityService.findOne('api::blog.blog', id, {
+            fields: ['title', 'description', 'createdAt', 'updatedAt'],
+        });
+    },
+
+    async createBlog(data) {
+        return strapi.entityService.create('api::blog.blog', {
+            data: {
+                title: data.title,
+                description: data.description,
+                publishedAt: new Date()
+            },
+        });
+    },
+
+    async updateBlog(id, data) {
+        return strapi.entityService.update('api::blog.blog', id, {
+            data: {
+                title: data.title,
+                description: data.description,
+                updatedAt: new Date()
+            },
+        });
+    },
+
+    async deleteBlog(id) {
+        return strapi.entityService.delete('api::blog.blog', id);
+    },
+}));
